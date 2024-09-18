c4df21e0b6b5f03fc3368b7ee662f0b194d2c45a
Diogo <dm.mateus@cmpus.fct.unl.pt>
2024-09-18 10:19:18 +0100

    Final version of the project (I think)

---
+++ ContactBookGit-master/src/Main.java
@@ -0,0 +1,197 @@
+import contactBook.Contact;
+import contactBook.ContactBook;
+
+import java.util.Scanner;
+
+/**
+ * @author Dimitrios Schoinas 65313 ; João Rivera 62877 e Diogo Mateus 65379
+ * Final version for the first project of Software Engineering
+ */
+
+public class Main {
+    //Constantes que definem os comandos
+    public static final String ADD_CONTACT    = "AC";
+    public static final String REMOVE_CONTACT = "RC";
+    public static final String GET_PHONE      = "GP";
+    public static final String GET_EMAIL      = "GE";
+    public static final String SET_PHONE      = "SP";
+    public static final String SET_EMAIL      = "SE";
+    public static final String LIST_CONTACTS  = "LC";
+    public static final String QUIT           = "Q";
+    public static final String GET_NAME       = "GN";
+    public static final String EXISTS_PHONE   = "EP";
+
+    //Constantes que definem as mensagens para o utilizador
+    public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
+    public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
+    public static final String CONTACT_ADDED = "contactBook.Contact added.";
+    public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
+    public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
+    public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String QUIT_MSG = "Goodbye!";
+    public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String CONTACTS_SHARE_NUMBER = "There are contacts that share phone numbers.";
+    public static final String CONTACTS_DIFFERENT_NUMBERS = "All contacts have different phone numbers.";
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ContactBook cBook = new ContactBook();
+        String comm = getCommand(in);
+
+        while (!comm.equals(QUIT)){
+            switch (comm) {
+                case ADD_CONTACT:
+                    addContact(in,cBook);
+                    break;
+                case REMOVE_CONTACT:
+                    deleteContact(in,cBook);
+                    break;
+                case GET_PHONE:
+                    getPhone(in,cBook);
+                    break;
+                case GET_EMAIL:
+                    getEmail(in,cBook);
+                    break;
+                case SET_PHONE:
+                    setPhone(in,cBook);
+                    break;
+                case SET_EMAIL:
+                    setEmail(in,cBook);
+                    break;
+                case LIST_CONTACTS:
+                    listAllContacts(cBook);
+                    break;
+                case GET_NAME:
+                    getName(in,cBook);
+                    break;
+                case EXISTS_PHONE:
+                    existsPhone(cBook);
+                        break;
+                default:
+                    System.out.println(COMMAND_ERROR);
+            }
+            System.out.println();
+            comm = getCommand(in);
+        }
+        System.out.println(QUIT_MSG);
+        System.out.println();
+        in.close();
+    }
+
+    private static String getCommand(Scanner in) {
+        String input;
+
+        input = in.nextLine().toUpperCase();
+        return input;
+    }
+
+    private static void addContact(Scanner in, ContactBook cBook) {
+        String name, email;
+        int phone;
+
+        name = in.nextLine();
+        phone = in.nextInt(); in.nextLine();
+        email = in.nextLine();
+        if (!cBook.hasContact(name)) {
+            cBook.addContact(name, phone, email);
+            System.out.println(CONTACT_ADDED);
+        }
+        else System.out.println(CONTACT_EXISTS);
+    }
+
+    private static void deleteContact(Scanner in, ContactBook cBook) {
+        String name;
+        name = in.nextLine();
+        if (cBook.hasContact(name)) {
+            cBook.deleteContact(name);
+            System.out.println(CONTACT_REMOVED);
+        }
+        else System.out.println(NAME_NOT_EXIST);
+    }
+
+    private static void getPhone(Scanner in, ContactBook cBook) {
+        String name;
+        name = in.nextLine();
+        if (cBook.hasContact(name)) {
+            System.out.println(cBook.getPhone(name));
+        }
+        else System.out.println(NAME_NOT_EXIST);
+    }
+
+    private static void getEmail(Scanner in, ContactBook cBook) {
+        String name;
+        name = in.nextLine();
+        if (cBook.hasContact(name)) {
+            System.out.println(cBook.getEmail(name));
+        }
+        else System.out.println(NAME_NOT_EXIST);
+    }
+
+    private static void setPhone(Scanner in, ContactBook cBook) {
+        String name;
+        int phone;
+        name = in.nextLine();
+        phone = in.nextInt(); in.nextLine();
+        if (cBook.hasContact(name)) {
+            cBook.setPhone(name,phone);
+            System.out.println(CONTACT_UPDATED);
+        }
+        else System.out.println(NAME_NOT_EXIST);
+    }
+
+    private static void setEmail(Scanner in, ContactBook cBook) {
+        String name;
+        String email;
+        name = in.nextLine();
+        email = in.nextLine();
+        if (cBook.hasContact(name)) {
+            cBook.setEmail(name,email);
+            System.out.println(CONTACT_UPDATED);
+        }
+        else System.out.println(NAME_NOT_EXIST);
+    }
+
+    private static void listAllContacts(ContactBook cBook) {
+        if (cBook.getNumberOfContacts() != 0) {
+            cBook.initializeIterator();
+            while( cBook.hasNext() ) {
+                Contact c = cBook.next();
+                System.out.println(c.getName() + "; " + c.getEmail() + "; " + c.getPhone());
+            }
+        }
+        else System.out.println(BOOK_EMPTY);
+    }
+
+    /**
+     * The operation receives a phone number and returns the contact with
+     * that given number. In case there are several contacts with
+     * that same phone number, the oldest contact is returned.
+     * @param cBook the contact book to retrieve the contacts from.
+     * @param in the Scanner object used to read user input.
+     **/
+    private static void getName(Scanner in, ContactBook cBook) {
+        int phone;
+        phone = in.nextInt(); in.nextLine();
+
+        //Checks if the Contact Book has that number
+        if(!cBook.hasPhone(phone) )
+            System.out.println(PHONE_NOT_EXIST);
+        else
+            System.out.println(cBook.getName(phone));
+            //If it has that number returns the contact associated
+    }
+
+    /**
+     * Check if there are contacts with the same phone number
+     * @param cBook the contact book to retrieve the contacts from.
+     **/
+    private static void existsPhone(ContactBook cBook) {
+        //Checks if the Contact Book has two identical numbers
+        if(cBook.existsPhone())
+            System.out.println(CONTACTS_SHARE_NUMBER);
+        else
+            System.out.println(CONTACTS_DIFFERENT_NUMBERS);
+    }
+}
+
